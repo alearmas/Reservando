f650cdeeaf8595f94ade186e57acd36c03ea9208
alearmas <aarmas17@gmail.com>
2019-05-16 00:08:17 -0300

    creacion de tests.js y vinculacion con test.html

---
+++ js/tests.js
@@ -0,0 +1,28 @@
+/* Testeo de función reservarHorario(horario)
+ * Cuando se reserva un horario de un restaurant, el horario correspondiente se elimina del arreglo.
+ * Cuando se reserva un horario que el restaurant no posee, el arreglo se mantiene igual.
+ * Cuando se intenta reservar un horario pero no se le pasa ningún parámetro a la función, el arreglo se mantiene igual.*/
+
+
+
+/* Testeo de función obtenerPuntuacion()
+ * Dado un restaurant con determinadas calificaciones, la puntuación (que es el promedio de ellas) se calcula correctamente.
+ * Dado un restaurant que no tiene ninguna calificación, la puntuación es igual a 0.*/
+
+
+
+//Testeo de función calificar()
+
+
+
+//Testeo de función buscarRestaurante(id)
+
+
+
+//Testeo de función obtenerRestaurante()
+
+
+
+//Testeo de función 
+
+
